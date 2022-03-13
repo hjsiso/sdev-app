a621ccffb67749a2a2d967176b4a099aa0555377
sisodev <hjsiso@gmail.com>
2022-03-13 07:57:59 -0500

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import "../app.css";
+  </script>
+  <div class="px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+    <slot />
+  </div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+    <script>
+        const toggleTheme = () => {
+            let currentTheme = document.documentElement.classList.contains('dark')
+            if(currentTheme) {
+                document.documentElement.classList.remove('dark')
+            }else{
+                document.documentElement.classList.add('dark')
+            }
+            console.log(currentTheme)
+        }
+
+    </script>
+    <div>
+      <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+      </span>
+    </div>
+    <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+      The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+    </p>
+
+    <button class="transition ease-in-out delay-350 hover:bg-indigo-600 bg-indigo-500 text-white text-sm rounded-md px-2 py-1" on:click="{toggleTheme}">Toggle Theme</button>
